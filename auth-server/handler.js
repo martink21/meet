a6a78c8f4b47a9369b55eeb178f4412bce5e3cfd
Martin Karastoyanov <martin.karastoyanov@gmail.com>
2021-07-29 12:18:45 +0200

    Update handler.js

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -17,7 +17,7 @@ const credentials = {
   token_uri: "https://oauth2.googleapis.com/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
   redirect_uris: ["https://martink21.github.io/meet"],
-  javascript_origins: ["https://martink21.github.io", "http://localhost:8080"],
+  javascript_origins: ["https://martink21.github.io", "http://localhost:3000"],
 };
 const { client_secret, client_id, redirect_uris, calendar_id } = credentials;
 const oAuth2Client = new google.auth.OAuth2(
