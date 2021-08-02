3b279730c905b1283df9de243fb876252f3d90d9
Martin Karastoyanov <martin.karastoyanov@gmail.com>
2021-08-02 23:53:26 +0200

    Create NumberOfEvents.js

---
+++ src/NumberOfEvents.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { ErrorAlert } from './Alert'; 
+
+function NumberOfEvents(props) {
+  const [ eventsToShow, setEventsToShow ] = useState(props.eventsToShow);
+  const text = props.text
+
+  const handleChange = (event) => {
+    if(event.target.value === "") {
+      setEventsToShow(event.target.value);
+      props.updateEventNum("NoNum");
+    } else {
+      setEventsToShow(event.target.value);
+      props.updateEventNum(event.target.value)
+    }
+  }
+
+    return (
+      <div className="numberOfEvents">
+        <ErrorAlert text={text}/>
+        <label htmlFor="number">Events per page: </label>
+          <input 
+          type="number" 
+          id="number" 
+          className="numberInput"
+          value={eventsToShow}
+          placeholder="#" 
+          onChange={handleChange} />
+      </div>
+    )
+}
+
+export default NumberOfEvents;
