bbb62f083b425c92a063eb94d96873eef9ceb23d
Martin Karastoyanov <martin.karastoyanov@gmail.com>
2021-08-06 01:48:24 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -52,8 +52,9 @@ class App extends Component {
     return (
       <div className="App">
         <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} numberOfEvents={this.numberOfEvents} />
-        <EventList events={this.state.events} />
         <NumberOfEvents updateNumberOfEvents={(e) => this.updateNumberOfEvents(e)} numberOfEvents={this.state.numberOfEvents}/>
+        <EventList events={this.state.events} />
+        
       </div>
     );
   }
