6295b3e292f693efb990824513dbe151ab7a5000
Martin Karastoyanov <martin.karastoyanov@gmail.com>
2021-08-19 00:13:12 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
-import { getEvents, extractLocations } from './api';
+import { getEvents, extractLocations, checkToken, getAccessToken } from './api';
 import './nprogress.css';
 import { WarningAlert } from './Alert';
+import WelcomeScreen from './WelcomeScreen';
 
 class App extends Component {
   constructor(props) {
@@ -15,7 +16,8 @@ class App extends Component {
       locations: [],
       numberOfEvents: 32,
       currentLocation: "all",
-      networkStatus: navigator.onLine ? 'Online' : 'Offline'
+      networkStatus: navigator.onLine ? 'Online' : 'Offline',
+      showWelcomeScreen: undefined
     };
   }
 
@@ -25,16 +27,24 @@ class App extends Component {
     this.updateEvents(this.state.currentLocation, eventNumber)
   }
 
-  componentDidMount() {
+  async componentDidMount() {
     this.mounted = true;
-    getEvents().then((events) => {
-      if (this.mounted) {
-        this.setState({ events, locations: extractLocations(events) });
-      }
-    });
+    const accessToken = localStorage.getItem('access_token');
+    const isTokenValid = (await checkToken(accessToken)).error ? false :
+      true;
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = searchParams.get("code");
+    this.setState({ showWelcomeScreen: !(code || isTokenValid) });
+    if ((code || isTokenValid) && this.mounted) {
+      getEvents().then((events) => {
+        if (this.mounted) {
+          this.setState({ events, locations: extractLocations(events) });
+        }
+      });
+    }
   }
 
-  componentWillUnmount(){
+  componentWillUnmount() {
     this.mounted = false;
   }
 
@@ -54,25 +64,29 @@ class App extends Component {
   };
 
   render() {
+    if (this.state.showWelcomeScreen === undefined) return <div className="App" />
     const { networkStatus } = this.state;
-      
+
     return (
       <div className="App">
         <WarningAlert
           text={networkStatus === 'Offline' ? 'App is running offline: data may not be updated' : ''}
         />
-        <CitySearch 
-        locations={this.state.locations} 
-        updateEvents={this.updateEvents} 
-        numberOfEvents={this.state.numberOfEvents} />
+        <CitySearch
+          locations={this.state.locations}
+          updateEvents={this.updateEvents}
+          numberOfEvents={this.state.numberOfEvents} />
 
-        <NumberOfEvents 
-        updateNumberOfEvents={(e) => this.updateNumberOfEvents(e)} 
-        numberOfEvents={this.state.numberOfEvents}/>
-        
-        <EventList 
-        events={this.state.events} />
-        
+        <NumberOfEvents
+          updateNumberOfEvents={(e) => this.updateNumberOfEvents(e)}
+          numberOfEvents={this.state.numberOfEvents} />
+
+        <EventList
+          events={this.state.events} />
+
+        <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen}
+          getAccessToken={() => { getAccessToken() }} />
+
       </div>
     );
   }
