20c8ca0438cde41bd3c59a1cf891e20135416157
Martin Karastoyanov <martin.karastoyanov@gmail.com>
2021-08-18 14:16:51 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import * as serviceWorker from './serviceWorker';
 import reportWebVitals from './reportWebVitals';
 import * as atatus from 'atatus-spa';
 
@@ -17,7 +17,7 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
 //serviceWorkerRegistration.unregister();
-serviceWorkerRegistration.register();
+serviceWorker.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
