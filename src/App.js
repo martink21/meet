a1bb104442be372a76fa0dc8fff0773edd6aa7ae
Martin Karastoyanov <martin.karastoyanov@gmail.com>
2021-08-22 00:04:27 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -95,7 +95,7 @@ class App extends Component {
         <h4>Events in each city</h4>
 
         <ScatterChart
-          width={400}
+          width={800}
           height={400}
           margin={{
             top: 20, right: 20, bottom: 20, left: 20,
